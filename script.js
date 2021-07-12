1d13c175c50cb71621be2a43086aee9b8406dba8
Spryti <67284103+Spryti@users.noreply.github.com>
2021-07-12 18:29:05 +0000

    First commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Author:
+// Author: Jordan Muturi
 
 // Global UI Variables
 let canvasDiv;
@@ -11,15 +11,35 @@ let textP2;
 let soundClassifier;
 
 function setup() {
-
+  canvasDiv = createDiv();
+  canvas = createCanvas(640, 480);
+  canvas.parent(canvasDiv);
+  textDiv = createDiv();
+  textP = createP("Model loading, please wait...");
+  textP.parent(textDiv);
+  textP2 = createP();
+  textP2.parent(textDiv);
+  //only shows 95%< confident options
+  let options = {
+    probabiltiyTreshhold: 0.95;
+  }
+  //load model = "model = ml5.model.featureExtractor"
+  soundClassifier = ml5.soundClassifier("SpeechCommands18w", options, modelReady);
 }
 
 function draw() {
-
+  let label = textP.html();
+  if(label.includes("up")) {
+    background(0, 255, 0);
+  } else if(label.includes("down")) {
+    background(255, 0, 0)
+  } else if(label.includes("down"))
 }
 
 function modelReady() {
-
+  textP.html("Model Ready. Say any of the commands below");
+  textP2.html("<b>Commads</b>: Digits 0-9, up, down, left, right, go, stop, yes, no");
+  soundClassifier.classify(gotResults);
 }
 
 function gotResults(error, results) {
@@ -27,8 +47,9 @@ function gotResults(error, results) {
     console.error(error);
   } else {
     // Complete the code below
-    let label;
-    let confidence;
+    let label = results[0].label;
+    let confidence = round(results[0].confidence, 2); * 100;
+    textP.html(label + ": " + confidence + "%");
 
   }
 }
